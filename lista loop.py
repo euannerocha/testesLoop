878bce92288878e3f44fed3d74a5ca230227e27d
Annelise Rocha <annelisesr@gmail.com>
2023-03-23 20:52:50 -0300

    first commit

---
+++ lista loop.py
@@ -0,0 +1,36 @@
+# FOR
+# lista = [2, 4, 6, 8, 9, 10, 12, 13]
+
+# for n in lista:
+#     if n % 2 != 0:
+#         continue
+#     elif n == 6:
+#         continue
+#     elif n == 8:
+#         continue
+#     else:
+#         print(n)
+
+# print("Deu tudo certo!")
+
+# def calcula(x):
+#     pass
+
+#WHILE
+# variavel = 1
+# lista = [1, 2, 3]
+
+# while variavel <= 10:
+#     print(variavel)
+#     variavel += 1
+#     lista.append(variavel)
+
+# print(lista)
+
+
+
+# lista = [2, 4, 6, 8, 9, 10, 12, 13]
+# tupla = (2, 4, 6, 8, 10, 12, 13)
+
+
+# print("Hello World!")
