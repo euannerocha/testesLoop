3df337490c9fcf48e182567b451a7388472248d3
Annelise Rocha <annelisesr@gmail.com>
2023-03-23 21:30:02 -0300

    Atualização

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document.querySelector(".buttonFeevale")
+console.log(button)
+
+button.addEventListener("click", () =>{
+    window.open("https://way.feevale.br/inscrevase/?utm_source=google&utm_medium=search&utm_campaign=vest2023&gclsrc=aw.ds")
+})
